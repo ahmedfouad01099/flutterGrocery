9f97564fa32cf28a71928904fbb672bcaeaa5014
Ahmed Mohamed Fouad <63275575+ahmedfouad01099@users.noreply.github.com>
2025-11-10 21:33:20 +0200

    finish designing of grocery market home screen including search bar, top selling list and near you shop list

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttergrocery/pages/home.dart';
 import 'package:fluttergrocery/pages/landing.dart';
 
 void main() {
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Landing(),
+      home: Home(),
     );
   }
 }
---
+++ lib/pages/home.dart
@@ -0,0 +1,423 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: 60, left: 20, right: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Image.asset(
+                          'images/wave.png',
+                          height: 30,
+                          width: 30,
+                          fit: BoxFit.cover,
+                        ),
+                        SizedBox(width: 5),
+                        Text(
+                          "Hey Alex,",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Eat fresh fruits and try\nto be healthy",
+                      style: TextStyle(
+                        color: Colors.black38,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    'images/ahmed.png',
+                    height: 60,
+                    width: 60,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 20),
+
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(left: 20, top: 5, bottom: 5),
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        color: Color(0xfff5f5f5),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Search Grocery",
+                          suffixIcon: Icon(Icons.search_outlined),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Top Selling",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "See All",
+                          style: TextStyle(
+                            color: Colors.black45,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+              
+                    SizedBox(height: 30),
+              
+                    Container(
+                      height: 237,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          Container(
+                            width: 190, // Add a fixed width for the card
+                            margin: EdgeInsets.only(
+                              right: 15,
+                            ), // Add some spacing between cards
+                            padding: EdgeInsets.only(
+                              top: 15,
+                              left: 20,
+                            ), // Add padding inside the card
+                            decoration: BoxDecoration(
+                              color: Color(0xffffe08e),
+                              borderRadius: BorderRadius.circular(
+                                30,
+                              ), // Optional: rounded corners
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Orange",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  "\$2.99 each",
+                                  style: TextStyle(
+                                    color: Colors.black45,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Center(
+                                  // Center the image
+                                  child: Image.asset(
+                                    'images/orange.png',
+                                    height: 100,
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment
+                                          .end, // This will now work properly
+                                  children: [
+                                    Container(
+                                      padding: EdgeInsets.all(10),
+                                      decoration: BoxDecoration(
+                                        color: Color(0xFFFFEAB5),
+                                        borderRadius: BorderRadius.only(
+                                          bottomRight: Radius.circular(30),
+                                          topLeft: Radius.circular(20),
+                                        ), // Optional: rounded corners
+                                      ),
+                                      child: Icon(
+                                        Icons.add,
+                                        color: Colors.orange,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+              
+                          SizedBox(width: 20),
+              
+                          Container(
+                            width: 190, // Add a fixed width for the card
+                            margin: EdgeInsets.only(
+                              right: 15,
+                            ), // Add some spacing between cards
+                            padding: EdgeInsets.only(
+                              top: 15,
+                              left: 20,
+                            ), // Add padding inside the card
+                            decoration: BoxDecoration(
+                              color: Color(0xffffe3e5),
+                              borderRadius: BorderRadius.circular(
+                                30,
+                              ), // Optional: rounded corners
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Apple",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  "\$1.99 each",
+                                  style: TextStyle(
+                                    color: Colors.black45,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Center(
+                                  // Center the image
+                                  child: Image.asset(
+                                    'images/apple.png',
+                                    height: 100,
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment
+                                          .end, // This will now work properly
+                                  children: [
+                                    Container(
+                                      padding: EdgeInsets.all(10),
+                                      decoration: BoxDecoration(
+                                        color: Color(0xffffeeeef),
+                                        borderRadius: BorderRadius.only(
+                                          bottomRight: Radius.circular(30),
+                                          topLeft: Radius.circular(20),
+                                        ), // Optional: rounded corners
+                                      ),
+                                      child: Icon(Icons.add, color: Colors.pink),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+              
+                    SizedBox(height: 20),
+              
+                    Text(
+                      "Near You",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "28+ shops",
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+              
+                    SizedBox(height: 10),
+              
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.asset(
+                            'images/grocery1.png',
+                            width: 120,
+                            height: 120,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+              
+                        SizedBox(width: 20),
+              
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Food 10 Ter3a ST.",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+              
+                            SizedBox(height: 5),
+              
+                            Text(
+                              "09:00AM - 10:00PM",
+                              style: TextStyle(
+                                color: Colors.black45,
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+              
+                            SizedBox(height: 5),
+              
+                            Row(
+                              children: [
+                                Icon(Icons.star, color: Colors.black),
+                                Text(
+                                  "4.9 | ",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Icon(Icons.location_on, color: Colors.black),
+                                Text(
+                                  "1.5Km",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+              
+                    SizedBox(height: 10),
+              
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.asset(
+                            'images/grocery2.png',
+                            width: 120,
+                            height: 120,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+              
+                        SizedBox(width: 20),
+              
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Mo Market Glaa ST.",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+              
+                            SizedBox(height: 5),
+              
+                            Text(
+                              "08:00AM - 08:00PM",
+                              style: TextStyle(
+                                color: Colors.black45,
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+              
+                            SizedBox(height: 5),
+              
+                            Row(
+                              children: [
+                                Icon(Icons.star, color: Colors.black),
+                                Text(
+                                  "4.5 | ",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Icon(Icons.location_on, color: Colors.black),
+                                Text(
+                                  "3.9Km",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
